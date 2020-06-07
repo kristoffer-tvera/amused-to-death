5974770a999df49311e229a28b293f1ae729afe5
Kristoffer Tverå <Bluekristoffer@gmail.com>
2020-06-07 02:02:21 +0200

    Added discord-bot to github proj

---
+++ jaina-proudmoore/bot.py
@@ -0,0 +1,45 @@
+# bot.py
+import os
+import discord
+import requests
+import random
+import string
+from dotenv import load_dotenv
+
+def randomString(stringLength=26):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.channel.id not in [718945308117303307, 433559219841531905]: #whitelist
+        return
+
+    if message.content == '!mudkip':
+        token = randomString()
+        id = message.author.name + '#' + message.author.discriminator
+
+        url = 'https://amusedtodeath.eu/boosting/api/bot/'
+        myobj = {'token': token, 'discord': id}
+
+        requests.post(url, data = myobj)
+
+        print(f'{id} was just serviced!')
+        await message.add_reaction('\U00002705')
+        await message.author.create_dm()
+        await message.author.dm_channel.send(
+            f'Hi, {id}. \nYou are on the list of approved users! \nTry logging in with this link: https://amusedtodeath.eu/boosting/api/auth/?token={token} \n\nThe link expires in 10 minutes, if it has expired by the time you read this, just ask for another link.'
+        )
+
+client.run(TOKEN)
