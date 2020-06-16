2207d93683e3ce32c868087191ae4fc6a2a8ef6b
Kristoffer Tverå <Bluekristoffer@gmail.com>
2020-06-17 01:49:10 +0200

    Updated unauthorized-page

--- boosting/api/unauthorized.php
+++ boosting/api/unauthorized.php
@@ -1,39 +1,66 @@
-<html lang="en" dir="ltr"><head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta http-equiv="Content-Language" content="en">
-    <meta name="msapplication-TileColor" content="#420420">
-    <meta name="theme-color" content="#420420">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="mobile-web-app-capable" content="yes">
-    <meta name="HandheldFriendly" content="True">
-    <meta name="MobileOptimized" content="320">
-    <link rel="icon" type="image/x-icon" href="/boosting/assets/favicon.ico" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/boosting/assets/images/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/boosting/assets/images/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/boosting/assets/images/favicon-16x16.png">
-    <link rel="shortcut icon" type="image/x-icon" href="/boosting/assets/favicon.ico" />
-    <title>Unauthorized - A2D</title>
-    <link href="/boosting/assets/css/dashboard.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,500,500i,600,600i,700,700i&amp;subset=latin-ext">
+<!doctype html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Unauthorized - A2D</title>
+  <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="">
+  <meta name="msapplication-TileColor" content="#206bc4">
+  <meta name="theme-color" content="#206bc4">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="mobile-web-app-capable" content="yes">
+  <meta name="HandheldFriendly" content="True">
+  <meta name="MobileOptimized" content="320">
+  <meta name="robots" content="noindex,nofollow,noarchive">
+  <link rel="icon" type="image/x-icon" href="/boosting/assets/favicon.ico" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/boosting/assets/images/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/boosting/assets/images/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/boosting/assets/images/favicon-16x16.png">
+  <link rel="shortcut icon" type="image/x-icon" href="/boosting/assets/favicon.ico" />
+  <!-- Libs CSS -->
+  <!-- Tabler Core -->
+  <link href="/boosting/assets/css/tabler.min.css" rel="stylesheet" />
+  <!-- Tabler Plugins -->
+  <link href="/boosting/assets/css/tabler-buttons.min.css" rel="stylesheet" />
+  <style>
+    body {
+      display: none;
+    }
+  </style>
 </head>
-  <body class="" style="background-color: #000">
-    <div class="page">
-      <div class="page-content">
-        <div class="container text-center">
-          <div class="display-1 text-muted mb-5"><i class="si si-exclamation"></i> 401</div>
-          <h1 class="h2 mb-3 text-muted">Unauthorized</h1>
-          <img src="/boosting/assets/images/jaina_dissapointed.gif" alt="Jaina Proudmore is dissapointed with you" class="w-100"/>
-          <p class="h4 text-muted font-weight-normal mb-7">Login through discord (!mudkip) if you want to change/update anything</p>
-          <a class="btn btn-primary" href="javascript:history.back()">
-            <i class="fe fe-arrow-left mr-2"></i>Go back
+<body class="antialiased theme-dark overflow-auto" style="background-color: #000">
+  <div class="flex-fill d-flex align-items-center justify-content-center">
+    <div class="container-xl py-6">
+      <div class="empty">
+        <div class="empty-icon">
+          <div class="display-4">401</div>
+        </div>
+
+        <h1 class="h2 text-muted">Unauthorized</h1>
+        
+        <img src="/boosting/assets/images/jaina_dissapointed.gif" alt="Jaina Proudmore is dissapointed with you" class="w-100"/>
+        <p class="empty-subtitle text-muted">
+          Login through discord (!mudkip) if you want to change/update anything
+        </p>
+        <div class="empty-action">
+          <a href="javascript:history.back()" class="btn btn-secondary">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z"/>
+              <line x1="5" y1="12" x2="19" y2="12" />
+              <line x1="5" y1="12" x2="11" y2="18" />
+              <line x1="5" y1="12" x2="11" y2="6" />
+            </svg>
+            Take me home
           </a>
         </div>
       </div>
     </div>
-  
+  </div>
+  <script>
+        document.body.style.display = "block"
+    </script>
 </body>
+
 </html>
