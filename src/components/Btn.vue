9d7b526c44eb95e7638c46803521b848c753773e
Amar Trebinjac <amartrebinjac@gmail.com>
2020-08-20 18:31:30 +0200

    Created button component & converted "TextField" to a generic input field.

---
+++ src/components/Btn.vue
@@ -0,0 +1,23 @@
+<template>
+    <button :type="type" class="button" :class="styles">{{text}}</button>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+    name: "Btn",
+    props: {
+        text: String,
+        type: String,
+        styles: String,
+    },
+})
+
+</script>
+
+<style lang="scss" scoped>
+.button {
+    padding: .5rem 1rem;
+}
+</style>
---
+++ src/components/InputField.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="input__group">
+    <label for="name">{{name}}</label>
+    <input class="input" :type="type" :name="name" :placeholder="text" :value="value" @input="(event) => $emit('update:value', event.target.value)" :min="min" :max="max" />
+  </div>
+</template>
+    
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TextField",
+  props: {
+    name: String,
+    text: String,
+    type: String,
+    value: String,
+    min: String,
+    max: String
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.input {
+  background-color: var(--background);
+  color: var(--primary-title-color);
+  box-sizing:border-box;
+  padding: .5rem;
+  border:none;
+  &__group {
+    display: flex;
+    flex-direction: column;
+    color: var(--primary-title-color);
+
+    & label {
+    margin-bottom: .5rem;
+    font-size: 1rem;
+    text-transform:capitalize;
+    }
+  }
+}
+</style>
