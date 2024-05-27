012ac7c8be13515cd0ed2dcb96dc6c231c102a64
Kristoffer Tverå <Bluekristoffer@gmail.com>
2024-05-27 11:27:27 +0200

    Fixed small issue with remaining legacy discord code

--- partials/_session_start.php
+++ partials/_session_start.php
@@ -1,6 +1,6 @@
 <?php session_start();
 
-if(!isset($_SESSION['auth']) && isset($_COOKIE['discord_auth']) && !empty($_COOKIE['discord_auth'])){
+if (!isset($_SESSION['auth']) && isset($_COOKIE['discord_auth']) && !empty($_COOKIE['discord_auth'])) {
 
     $auth_invalid = false;
 
@@ -8,7 +8,7 @@ if(!isset($_SESSION['auth']) && isset($_COOKIE['discord_auth']) && !empty($_COOK
 
     include_once './api/secrets.php';
 
-    $curl = curl_init( 'https://discord.com/api/oauth2/token' );
+    $curl = curl_init('https://discord.com/api/oauth2/token');
     $headers[] = 'Accept: application/json';
     $params = [
         'grant_type' => 'refresh_token',
@@ -19,15 +19,15 @@ if(!isset($_SESSION['auth']) && isset($_COOKIE['discord_auth']) && !empty($_COOK
         'scope' => 'identify'
     ];
 
-    curl_setopt( $curl, CURLOPT_POST, true);
-    curl_setopt( $curl, CURLOPT_POSTFIELDS, $params);
-    curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt( $curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
-    $response = curl_exec( $curl );
+    $response = curl_exec($curl);
 
-    $status = curl_getinfo( $curl, CURLINFO_HTTP_CODE);
-    if($status != 200){
+    $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    if ($status != 200) {
         exit;
     }
 
@@ -36,32 +36,31 @@ if(!isset($_SESSION['auth']) && isset($_COOKIE['discord_auth']) && !empty($_COOK
     $accessToken = $decoded->access_token;
     $refreshToken = $decoded->refresh_token;
     $headers[] = 'Authorization: Bearer ' . $accessToken;
-    curl_setopt ($curl, CURLOPT_URL, 'https://discord.com/api/users/@me');
-    curl_setopt( $curl, CURLOPT_POST, false);
-    curl_setopt( $curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_URL, 'https://discord.com/api/users/@me');
+    curl_setopt($curl, CURLOPT_POST, false);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
-    $response = curl_exec( $curl );
+    $response = curl_exec($curl);
     $user = json_decode($response);
 
-    curl_close( $curl );
+    curl_close($curl);
 
-    $discord_username = $user->username . '#' . $user->discriminator;
+    $discord_username = $user->username;
 
-    if(empty($discord_username)) $auth_invalid = true;
+    if (empty($discord_username)) $auth_invalid = true;
 
-    if(!$auth_invalid){
+    if (!$auth_invalid) {
         $_SESSION['auth'] = $discord_username;
-        if(in_array($discord_username, $admins)){
+        if (in_array($discord_username, $admins)) {
             $_SESSION['admin'] = true;
         }
-        setcookie('discord_auth', $refreshToken, time() + 60*60*24*365, '/');
+        setcookie('discord_auth', $refreshToken, time() + 60 * 60 * 24 * 365, '/');
     } else {
         setcookie('discord_auth', null, -1, '/');
     }
-    
 }
 
-if(!isset($_SESSION['auth']) && isset($_COOKIE['auth']) && !empty($_COOKIE['auth'])){
+if (!isset($_SESSION['auth']) && isset($_COOKIE['auth']) && !empty($_COOKIE['auth'])) {
 
     $auth_invalid = false;
 
@@ -72,31 +71,26 @@ if(!isset($_SESSION['auth']) && isset($_COOKIE['auth']) && !empty($_COOKIE['auth
     $conn = new mysqli($dbservername, $dbusername, $dbpassword, $dbname);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
-    } 
+    }
 
     $stmt = $conn->prepare("SELECT * FROM `$dbtable_auth` WHERE token=?");
     $stmt->bind_param('s', $auth);
     $stmt->execute();
     $result = $stmt->get_result();
-    
+
     $auth = $result->fetch_assoc();
 
-    if(empty($auth)) $auth_invalid = true;
+    if (empty($auth)) $auth_invalid = true;
 
-    if(!$auth_invalid && strtotime($auth['expire_date']) < strtotime("now")) $auth_invalid = true;
+    if (!$auth_invalid && strtotime($auth['expire_date']) < strtotime("now")) $auth_invalid = true;
 
-    if(!$auth_invalid){
+    if (!$auth_invalid) {
         $discord_username = $auth['discord'];
         $_SESSION['auth'] = $discord_username;
-        if(in_array($discord_username, $admins)){
+        if (in_array($discord_username, $admins)) {
             $_SESSION['admin'] = true;
         }
     } else {
         setcookie('auth', null, -1, '/');
     }
-    
-    
 }
-
-
-?>
