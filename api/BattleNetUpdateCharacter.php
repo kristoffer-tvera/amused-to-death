cebb01336ee8dd0945136937a2a6974ac62d686d
Kristoffer Tverå <Bluekristoffer@gmail.com>
2024-06-12 16:50:44 +0200

    mb_strtolower converted to strtolower

--- api/BattleNetUpdateCharacter.php
+++ api/BattleNetUpdateCharacter.php
@@ -1,93 +1,92 @@
 <?php session_start();
 
-    if(empty($_SESSION['auth'])){
-        http_response_code(401);
-        require './unauthorized.php';
-        exit;
-    }
+if (empty($_SESSION['auth'])) {
+    http_response_code(401);
+    require './unauthorized.php';
+    exit;
+}
 
-    include_once 'secrets.php';
-    $ajax = false;
-    if(isset($_GET["ajax"]) && !empty($_GET["ajax"])){
-        $ajax = true;
-    }
-    
-    $return = '';
-    if(isset($_GET["return"]) && !empty($_GET["return"])){
-        $return = htmlspecialchars($_GET["return"]);
-    } else {
-        $return = "/";
-    }
+include_once 'secrets.php';
+$ajax = false;
+if (isset($_GET["ajax"]) && !empty($_GET["ajax"])) {
+    $ajax = true;
+}
 
-    $id = '';
-    if(isset($_GET["id"]) && !empty($_GET["id"])){
-        $id = strtolower(htmlspecialchars($_GET["id"]));
-    }
+$return = '';
+if (isset($_GET["return"]) && !empty($_GET["return"])) {
+    $return = htmlspecialchars($_GET["return"]);
+} else {
+    $return = "/";
+}
 
-    $conn = new mysqli($dbservername, $dbusername, $dbpassword, $dbname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    } 
+$id = '';
+if (isset($_GET["id"]) && !empty($_GET["id"])) {
+    $id = strtolower(htmlspecialchars($_GET["id"]));
+}
 
-    $stmt = $conn->prepare("SELECT * FROM `$dbtable_characters` WHERE id=?");
-    $stmt->bind_param('i', $id);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    
-    $char = $result->fetch_assoc();
-    
-    if(empty($char)){
-        http_response_code(404);
-        exit;
-    }
+$conn = new mysqli($dbservername, $dbusername, $dbpassword, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-    $name = urlencode(mb_strtolower($char["name"]));
-    $realm = strtolower($char["realm"]);
-    //=======================================================================================================
-    // Blizzard oAuth endpoint URL
-    //=======================================================================================================
+$stmt = $conn->prepare("SELECT * FROM `$dbtable_characters` WHERE id=?");
+$stmt->bind_param('i', $id);
+$stmt->execute();
+$result = $stmt->get_result();
 
-    $url = "https://eu.api.blizzard.com/profile/wow/character/" . $realm . "/" . $name . "?namespace=profile-eu&locale=en_GB&access_token=" ;
+$char = $result->fetch_assoc();
 
-    $ch = curl_init( $url . $_SESSION['token']);
-    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true);
+if (empty($char)) {
+    http_response_code(404);
+    exit;
+}
 
-    $response = curl_exec( $ch );
-    $status = curl_getinfo( $ch, CURLINFO_HTTP_CODE);
+$name = urlencode(strtolower($char["name"]));
+$realm = strtolower($char["realm"]);
+//=======================================================================================================
+// Blizzard oAuth endpoint URL
+//=======================================================================================================
 
-    curl_close( $ch );
+$url = "https://eu.api.blizzard.com/profile/wow/character/" . $realm . "/" . $name . "?namespace=profile-eu&locale=en_GB&access_token=";
 
-    if ($status !== 200 && $ajax) {
-        http_response_code($status);
-        echo $name;
-        exit;
-    } else if ($status !== 200){
-        header('Location: ' . $return . '&error=true');
-    }
+$ch = curl_init($url . $_SESSION['token']);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-    $decoded = json_decode($response);
+$response = curl_exec($ch);
+$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+curl_close($ch);
+
+if ($status !== 200 && $ajax) {
+    http_response_code($status);
+    echo $name;
+    exit;
+} else if ($status !== 200) {
+    header('Location: ' . $return . '&error=true');
+}
+
+$decoded = json_decode($response);
 
 
-    $ilvl = $decoded->average_item_level;
+$ilvl = $decoded->average_item_level;
 
-    $stmt = $conn->prepare("UPDATE `$dbtable_characters` SET ilvl=? WHERE id=?");
-    $stmt->bind_param('ii', $ilvl, $id);
-    $stmt->execute();
+$stmt = $conn->prepare("UPDATE `$dbtable_characters` SET ilvl=? WHERE id=?");
+$stmt->bind_param('ii', $ilvl, $id);
+$stmt->execute();
 
-    if(!$ajax){
-        // Query Logging
-        $request = 'BNET => ' . $url . 'SECRET';
-        $log = $conn->prepare("INSERT INTO `$dbtable_log` (query, user) VALUES (?, ?)");
-        $log->bind_param('ss', $request, $_SESSION['auth']);
-        $log->execute();
-    }
-    
-    if($ajax){
-        http_response_code(200);
-        echo $name;
-        exit;
-    } else {
-        header('Location: ' . $return);
-    }
-    // exit;
-?>
+if (!$ajax) {
+    // Query Logging
+    $request = 'BNET => ' . $url . 'SECRET';
+    $log = $conn->prepare("INSERT INTO `$dbtable_log` (query, user) VALUES (?, ?)");
+    $log->bind_param('ss', $request, $_SESSION['auth']);
+    $log->execute();
+}
+
+if ($ajax) {
+    http_response_code(200);
+    echo $name;
+    exit;
+} else {
+    header('Location: ' . $return);
+}
+// exit;
