eb357c44c166a8e05f0676d619d3ec84f8df6954
Kristoffer Tverå <Bluekristoffer@gmail.com>
2026-07-03 01:20:13 +0200

    Cleanup in mismatch between class ids and numbers

--- frontend/src/data/classes.ts
+++ frontend/src/data/classes.ts
@@ -5,20 +5,20 @@ export interface WowClass {
 }
 
 export const wowClasses: WowClass[] = [
-    { id: 0, name: "Unknown", color: "#808080" },
-    { id: 1, name: "Druid", color: "#FF7C0A" },
-    { id: 2, name: "Paladin", color: "#F48CBA" },
-    { id: 3, name: "Warrior", color: "#C69B6D" },
-    { id: 4, name: "Demon Hunter", color: "#A330C9" },
-    { id: 5, name: "Hunter", color: "#AAD372" },
-    { id: 6, name: "Mage", color: "#3FC7EB" },
-    { id: 7, name: "Rogue", color: "#FFF468" },
-    { id: 8, name: "Death Knight", color: "#C41E3A" },
-    { id: 9, name: "Priest", color: "#FFFFFF" },
-    { id: 10, name: "Warlock", color: "#8788EE" },
-    { id: 11, name: "Shaman", color: "#0070DD" },
-    { id: 12, name: "Monk", color: "#00FF98" },
-    { id: 13, name: "Evoker", color: "#33937F" },
+    { id: -1, name: "Unknown", color: "#808080" },
+    { id: 0, name: "Druid", color: "#FF7C0A" },
+    { id: 1, name: "Paladin", color: "#F48CBA" },
+    { id: 2, name: "Warrior", color: "#C69B6D" },
+    { id: 3, name: "Demon Hunter", color: "#A330C9" },
+    { id: 4, name: "Hunter", color: "#AAD372" },
+    { id: 5, name: "Mage", color: "#3FC7EB" },
+    { id: 6, name: "Rogue", color: "#FFF468" },
+    { id: 7, name: "Death Knight", color: "#C41E3A" },
+    { id: 8, name: "Priest", color: "#FFFFFF" },
+    { id: 9, name: "Warlock", color: "#8788EE" },
+    { id: 10, name: "Shaman", color: "#0070DD" },
+    { id: 11, name: "Monk", color: "#00FF98" },
+    { id: 12, name: "Evoker", color: "#33937F" },
 ];
 
 export function getClassById(id: number): WowClass {
